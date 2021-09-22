6fd0366b33470b20966ef13aedcf09730236893a
Joshua Rawlings <joshua.rawlings@outlook.com>
2021-09-23 00:59:25 +0100

    final version - ticker list consisting of the 101st and 350 largest shares by market cap listed on LSE main market

--- uk-ticker/uk-ticker.py
+++ uk-ticker/uk-ticker.py
@@ -20,39 +20,25 @@ def get_link(url: object):
 
 # excel links
 link1 = get_link('https://www.londonstockexchange.com/reports?tab=instruments')
-link2 = get_link('https://www.londonstockexchange.com/reports?tab=equities')
 
 # load london stock exchange instruments and ticker lists into dataframes
-_df = pd.read_excel(link1, header=7, usecols="A:O")
-_df2 = pd.read_excel(link2, header=7, usecols="B:O")
+_df = pd.read_excel(link1, header=7, sheet_name='1.1 Shares', usecols="A:O")
 
-# TO COMPLETE
+# process and clean dataset
+df = _df[['TIDM', 'Issuer Name', 'ICB Super-Sector Name', 'Trading Currency', 'Country of Incorporation',
+          'Security Mkt Cap (in £m)', 'LSE Market']]
 
-tick_list = _df[['TIDM', 'Issuer Name', 'Instrument Name', 'ICB Super-Sector Name', 'Trading Currency',
-                 'Country of Incorporation', 'Market Sector Code']]
+df = df.rename(columns={'TIDM': 'ticker', 'Issuer Name': 'name', 'ICB Super-Sector Name': 'industry',
+                        'Trading Currency': 'currency', 'Country of Incorporation': 'country',
+                        'LSE Market': 'code', 'Security Mkt Cap (in £m)': 'cap'})
 
-df = pd.read_excel("https://bit.ly/2QLIy2z", header=5, usecols="B:J")
-mrk_cap = df[['Company Name', 'Company Market Cap (£m)']]
-mrk_cap = mrk_cap.rename(columns={'Company Name': 'Issuer Name'})
+df = df[df['currency'] == 'GBX']
+df = df[df['code'] == 'MAIN MARKET']
+df['cap'] = df['cap'].astype(float)
+df = df.sort_values(by='cap', ascending=False)
+df = df.dropna().reset_index(drop=True)
 
-uk_list = pd.merge(tick_list, mrk_cap, how='left', on="Issuer Name")
-
-uk_list = uk_list.rename(columns={'TIDM': 'ticker', 'Issuer Name': 'name', 'Instrument Name': 'instrument',
-                                  'ICB Super-Sector Name': 'industry', 'Trading Currency': 'currency',
-                                  'Country of Incorporation': 'country', 'Market Sector Code': 'code',
-                                  'Company Market Cap (£m)': 'cap'})
-# clean ticker data
-uk_list = uk_list[uk_list['country'] == 'United Kingdom']
-uk_list = uk_list[uk_list['currency'] == 'GBX']
-uk_list = uk_list[uk_list['code'].str.contains('F')]
-
-uk_list['cap'] = uk_list['cap'].replace(['-'], ['NaN'])
-uk_list['cap'] = uk_list['cap'].astype(float)
-
-uk_list = uk_list.sort_values(by='cap', ascending=False)
-uk_list['ticker'] = uk_list['ticker'].str.replace('.', '')
-uk_list = uk_list.dropna()
-
-del mrk_cap
-del tick_list
-del df
+# final ticker list
+ftse250 = df.iloc[100:350, :]
+ftse250 = ftse250.sort_values(by='ticker', ascending=True)
+ftse250 = ftse250[['ticker', 'name', 'industry']].reset_index(drop=True)
